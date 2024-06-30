de9833cebee4f92be43dae11dafc98fe43e56a46
Sopvia <112272251+Sopvia@users.noreply.github.com>
2024-06-30 13:29:55 +0200

    added a GUI with customtkinter

--- RPS.py
+++ RPS.py
@@ -1,51 +1,114 @@
 from random import randint
-from tkinter import *
+import tkinter as tk
+import customtkinter
 
-window = Tk()
 
-img = PhotoImage(file='ribbon.png')
-window.wm_iconphoto(False, img)
+class window(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs, fg_color='#FFF3F5')
 
-window.title('Rock Paper Scissors (cute version)')
-window.geometry("500x500")
+        self.font = customtkinter.CTkFont(family='Georgia', size=12)
 
-# title = Label(window, text="Rock Paper Scissors").grid(row=0, column=4)
+        self.player = customtkinter.CTkLabel(self, text="Player", text_color='#FF92A5', font=(self.font, 14), height=8, width=8)
+        self.player.grid(row=0, column=0, padx=20, sticky="ew")
 
-player = Label(window, text="Player").grid(row=1, column=1)
-vs = Label(window, text="vs").grid(row=1, column=2)
-computer = Label(window, text="Computer").grid(row=1, column=3)
+        self.vs = customtkinter.CTkLabel(self, text="vs", text_color='#FF92A5', font=(self.font, 14), height=8, width=8)
+        self.vs.grid(row=0, column=1, padx=20, sticky="ew")
 
-result = Label(window, text="").grid(row=2, column=1)
+        self.computer = customtkinter.CTkLabel(self, text="Computer", text_color='#FF92A5', font=(self.font, 14), height=8, width=8)
+        self.computer.grid(row=0, column=2, padx=20, sticky="ew")
 
-rock = Button(window, text="Rock", width=8, cursor="heart", bg="pink").grid(row=3, column=1, padx=5, pady=5)
-paper = Button(window, text="Paper", width=8, cursor="heart", bg="pink").grid(row=3, column=2, padx=5, pady=5)
-scissors = Button(window, text="Scissors", width=8, cursor="heart", bg="pink").grid(row=3, column=3, padx=5, pady=5)
+        self.rock = customtkinter.CTkButton(self, text="Rock", text_color="white", font=(self.font, 14), height=8, width=8, fg_color="#FFC0CB", hover_color="#FF92A5", command=lambda: action("Rock"))
+        self.rock.grid(row=3, column=0, padx=20, sticky="ew")
 
-t = ["Rock", "Paper", "Scissors"]
-computer = t[randint(0,2)]
-player = False
+        self.paper = customtkinter.CTkButton(self, text="Paper", text_color="white", font=(self.font, 14), height=8, width=8, fg_color="#FFC0CB", hover_color="#FF92A5", command=lambda: action("Paper"))
+        self.paper.grid(row=3, column=1, padx=20, sticky="ew")
 
-while player == False:
-    player = input("Rock, Paper, Scissors?")
-    if player == computer:
-        print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
-        else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
-        else:
-            print("You win!", player, "covers", computer)
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose!", computer, "smashes", player)
-        else:
-            print("You win!", player, "cut", computer)
-    else:
-        print("That's not a valid play. Check your spelling!")
+        self.scissors = customtkinter.CTkButton(self, text="Scissors", text_color="white", font=(self.font, 14), height=8, width=8, fg_color="#FFC0CB", hover_color="#FF92A5", command=lambda: action("Scissors"))
+        self.scissors.grid(row=3, column=2, padx=20, sticky="ew")
 
-player = False
-computer = t[randint(0,2)]
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=2)
+        self.grid_rowconfigure(2, weight=3)
+        self.grid_rowconfigure(3, weight=3)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+
+
+        def action(x):
+            self.rock.grid_forget()
+            self.paper.grid_forget()
+            self.scissors.grid_forget()
+
+            self.result_player = customtkinter.CTkLabel(self, text_color='#FF92A5', font=(self.font, 14))
+            self.result_player.grid(row=1, column=0, padx=20, sticky='ew')
+
+            self.result_computer = customtkinter.CTkLabel(self, text_color='#FF92A5', font=(self.font, 14))
+            self.result_computer.grid(row=1, column=2, padx=20, sticky='ew')
+
+            self.outcome = customtkinter.CTkLabel(self, text_color='#FF92A5', font=(self.font, 14))
+            self.outcome.grid(row=2, column=1, padx=20, sticky='ew')
+            
+            choice = ["Rock", "Paper", "Scissors"]
+            computer = choice[randint(0,2)]
+            match x:
+                case "Rock":
+                    player = "Rock"
+                case "Paper":
+                    player = "Paper"
+                case "Scissors":
+                    player = "Scissors"
+
+            if player == computer:
+                outcome_text = 'Tie!'
+            elif player == "Rock":
+                if computer == "Paper":
+                    outcome_text = 'You lose! Computer covers player'
+                else:
+                    outcome_text = 'You win! Player smashes computer'
+            elif player == "Paper":
+                if computer == "Scissors":
+                    outcome_text = 'You lose! Computer cuts player'
+                else:
+                    outcome_text = 'You win! Player covers computer'
+            elif player == "Scissors":
+                if computer == "Rock":
+                    outcome_text = 'You lose! Computer smashes player'
+                else:
+                    outcome_text = 'You win! Player cuts computer'
+
+            self.result_player.configure(text=player)
+            self.result_computer.configure(text=computer)
+            self.outcome.configure(text=outcome_text)
+
+            self.restart = customtkinter.CTkButton(self, text="Try again", text_color="white", font=(self.font, 14), fg_color="#FFC0CB", hover_color="#FF92A5", command=tryagain, height=8, width=8)
+            self.restart.grid(row=3, column=1, padx=20, sticky="ew")
+
+        def tryagain():
+            self.result_player.configure(text='')
+            self.result_computer.configure(text='')
+            self.outcome.configure(text='')
+
+            self.restart.grid_forget()
+            self.rock.grid(row=3, column=0, padx=20, sticky="ew")
+            self.paper.grid(row=3, column=1, padx=20, sticky="ew")
+            self.scissors.grid(row=3, column=2, padx=20, sticky="ew")
+
+
+class root(customtkinter.CTk):
+    def __init__(self):
+        super().__init__(fg_color="white")
+        self.geometry("500x500")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.title('Rock Paper Scissors (cute version)')
+        self.iconbitmap('icon.ico')
+
+        self.window_frame = window(master=self)
+        self.window_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+
+app = root()
+app.mainloop()
